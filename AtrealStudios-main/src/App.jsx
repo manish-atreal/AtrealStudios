6387fd02e0100d3a5114871891e3562574b5a90e
manish-atreal <manishatreal@gmail.com>
2026-03-16 14:57:57 +0530

    Refactor App component to include LayoutWrapper

--- AtrealStudios-main/src/App.jsx
+++ AtrealStudios-main/src/App.jsx
@@ -1,47 +1,61 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from '../src/components/navbar.jsx';
-import Home from './Home.jsx';  
-import About from './About.jsx';
-import Career from './Career.jsx';
-import Contact from './Contact.jsx';
-import Footer from './components/footer.jsx';
-import Architect_interior_designers from './service-pages/architect_interior_designers.jsx';
-import Enterprises from './service-pages/enterprises.jsx';
-import Healthcare_Education from './service-pages/healthcare_education.jsx';
-import Builders_developers from './service-pages/builders_developers.jsx';
-import Services from './Services.jsx';
-import Team from './Team.jsx';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
+
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Career from "./Career.jsx";
+import Contact from "./Contact.jsx";
+import Services from "./Services.jsx";
+import Team from "./Team.jsx";
+import Viewer from "./Viewer.jsx";
+
+import Architect_interior_designers from "./service-pages/architect_interior_designers.jsx";
+import Enterprises from "./service-pages/enterprises.jsx";
+import Healthcare_Education from "./service-pages/healthcare_education.jsx";
+import Builders_developers from "./service-pages/builders_developers.jsx";
+
+
+function LayoutWrapper() {
+  const location = useLocation();
+
+  const hideLayout = location.pathname === "/services/viewer";
 
   return (
     <>
-    
-     <Router>
-    <h1>Atreal Studios</h1>
-     <Navbar />
-     <div className="pt-20"> {/* Add padding to prevent overlap */}
-       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/about" element={<About />} />
-         <Route path="/career" element={<Career />} />
-         <Route path="/contact" element={<Contact />} />
-         <Route path="/services/architect-interior-designers" element={<Architect_interior_designers />} />
-         <Route path="/services/business&enterprises" element={<Enterprises />} />
-         <Route path="/services/builders-developers" element={<Builders_developers/>} />
-         <Route path="/services/healthcare&education" element={<Healthcare_Education/>} />
-         <Route path="/services" element={<Services/>} />
-         <Route path="/team" element={<Team/>} />
-       </Routes>
-     </div>
-     <Footer />
-   </Router>
-     </>
-  );
-  
+      {!hideLayout && <Navbar />}
 
+      <div className={!hideLayout ? "pt-20" : ""}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/team" element={<Team />} />
+
+          <Route path="/services/architect-interior-designers" element={<Architect_interior_designers />} />
+          <Route path="/services/business&enterprises" element={<Enterprises />} />
+          <Route path="/services/builders-developers" element={<Builders_developers />} />
+          <Route path="/services/healthcare&education" element={<Healthcare_Education />} />
+
+          <Route path="/services/viewer" element={<Viewer />} />
+        </Routes>
+      </div>
+
+      {!hideLayout && <Footer />}
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <LayoutWrapper />
+    </Router>
+  );
+}
+
+export default App;
